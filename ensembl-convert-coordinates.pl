db58212bda9fccc445aea192121dc2d7aee0079c
Natalie Willhoft <nataliewillhoft@natalies-mbp.home>
2020-02-01 15:01:47 +0000

    Load ensembl modules, load test registry and print test db adaptor info.

--- ensembl-convert-coordinates.pl
+++ ensembl-convert-coordinates.pl
@@ -1,22 +1,62 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 
 use strict;
 use warnings;
 
-# ensembl-related modules
+use FindBin;
+use DBD::mysql;
+use Data::Dumper;
+
+# load ensembl-related modules
+use lib "$FindBin::Bin/src/bioperl-1.6.924";
+use lib "$FindBin::Bin/src/ensembl/modules";
+use lib "$FindBin::Bin/src/ensembl-compara/modules";
+use lib "$FindBin::Bin/src/ensembl-variation/modules";
+use lib "$FindBin::Bin/src/ensembl-funcgen/modules";
+
 use Bio::EnsEMBL::Registry;
 use Bio::EnsEMBL::Utils::ConfigRegistry;
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
 
-# connect to human core db v98
-new Bio::EnsEMBL::DBSQL::DBAdaptor(
-  -host    => 'ensembldb.ensembl.org',
-  -user    => 'anonymous',
-  -port    => '3306',
-  -species => 'homo_sapiens',
-  -group   => 'core',
-  -dbname  => 'homo_sapiens_core_98_38'
+
+# get a new registry object
+my $registry = 'Bio::EnsEMBL::Registry';
+
+# load registry from multiple databases
+$registry->load_registry_from_multiple_dbs(
+    {
+      -host    => 'ensembldb.ensembl.org',
+      -user    => 'anonymous',
+      -port    => '3306',
+      -species => 'homo_sapiens',
+      -group   => 'core',
+      -dbname  => 'homo_sapiens_core_98_38'
+    },
+    {
+      -host => 'ensembldb.ensembl.org',
+      -user => 'anonymous',
+      -port => 3337
+    }
 );
 
-# create another connection for v37
+my @db_adaptors = @{ $registry->get_all_DBAdaptors() };
+
+foreach my $db_adaptor (@db_adaptors) {
+    my $db_connection = $db_adaptor->dbc();
+
+    printf(
+        "species/group\t%s/%s\ndatabase\t%s\nhost:port\t%s:%s\n\n",
+        $db_adaptor->species(),   $db_adaptor->group(),
+        $db_connection->dbname(), $db_connection->host(),
+        $db_connection->port()
+    );
+}
+
+# print "registry: $registry\n";
+
+
+# get a slice adaptor for the human core database
+my $slice_adaptor = $registry->get_adaptor( 'Human', 'Core', 'Slice' );
+
+# print Dumper( $slice_adaptor);
